953f00968c1420760d77caf33e4b9c62567bb4e2
Graham Hukill <ej2929@vmlib2.lib.wayne.edu>
2015-04-14 10:53:07 -0400

    updated report filename generation

--- crud/export_csv.php
+++ crud/export_csv.php
@@ -5,28 +5,48 @@ include('../inc/functions.php');
 $_REQUEST = json_decode($_REQUEST['params'],true);
 
 
-// default to ALL
-if ( isset($_REQUEST['locations']) && $_REQUEST['locations'] == array("ALL")){
-	$location_where = "location = ANY(select location from ref_stats)";
-}
-elseif ( isset($_REQUEST['locations']) ) {
-	$location_where = "location IN ('".implode("', '",$_REQUEST['locations'])."')";
-	// adjust for combined locations
-	if ( in_array("PK_COMB", $_REQUEST['locations'])){
-		$location_where = str_replace("'PK_COMB'", "'PK1','PK2'", $location_where);
+$selected_locations = array();
+	// default to ALL
+	if ( isset($_REQUEST['locations']) && $_REQUEST['locations'] == array("ALL")){
+		$location_where = "location = ANY(select location from ref_stats)";
+		$selected_locations = $simple_location_array; // from config.php
 	}
-	if ( in_array("MAIN_CAMPUS", $_REQUEST['locations'])){
-		$location_where = str_replace("'MAIN_CAMPUS'", "'PK1','PK2','UGL'", $location_where);
+
+	elseif ( isset($_REQUEST['locations']) ) {
+		
+		// prepare SQL clause
+		$location_where = "location IN ('".implode("', '",$_REQUEST['locations'])."')";
+
+		// // prepare selected_locations
+		foreach($_REQUEST['locations'] as $location){
+			if ($location != "NOPE" && $location != "ALL" && $location != "MAIN_CAMPUS"){
+				array_push($selected_locations, $location);
+			}
+		}
+
+		// adjust for combined locations
+		if ( in_array("PK_COMB", $_REQUEST['locations'])){
+			$location_where = str_replace("'PK_COMB'", "'PK1','PK2'", $location_where);
+			array_push($selected_locations, "PK1","PK2");
+		}
+		if ( in_array("MAIN_CAMPUS", $_REQUEST['locations'])){
+			$location_where = str_replace("'MAIN_CAMPUS'", "'PK1','PK2','UGL'", $location_where);
+			array_push($selected_locations, "PK1", "PK2", "UGL");
+		}
 	}
-}
-else {
-	$location_where = "location = {$_COOKIE['location']}";
-}	
 
+	else {
+		$location_where = "location = {$_COOKIE['location']}";
+		$selected_locations = array($_COOKIE['location']);
+	}	
 
-// get date limitiers
-$date_start = date("Y-m-d", strtotime($_REQUEST['date_start']));
-$date_end = date("Y-m-d", strtotime($_REQUEST['date_end']));
+	// finish cleaning $selected_locations
+	$selected_locations = array_unique($selected_locations);
+	$selected_locations_string = implode('_',$selected_locations);
+
+	// get date limitiers
+	$date_start = date("Y-m-d", strtotime($_REQUEST['date_start']));
+	$date_end = date("Y-m-d", strtotime($_REQUEST['date_end']));
 
 
 // All transactions in date range (appropriate for csv export)
@@ -36,7 +56,7 @@ $result = mysqli_query($link, $full_query) or trigger_error(mysqli_error());
 $fp = fopen('php://output', 'w');
 if ($fp && $result) {
     header('Content-Type: text/csv');
-    header('Content-Disposition: attachment; filename="export.csv"');
+    header("Content-Disposition: attachment; filename='RefStats_Report-$date_start_$date_end-$selected_locations_string.csv'");
     
     // write column names
     $fields = mysqli_num_fields ( $result );
