c0843078a700a59e8ddcf51c7938f66fe28e113f
Cole Hudson <cole.hudson@wayne.edu>
2016-04-01 14:13:30 -0400

    updating buttons

--- index.php
+++ index.php
@@ -163,8 +163,8 @@ global $user_arrays;
 					echo $button;
 				}
 			}
-			print_r($location_array);
-			echo $_COOKIE['location'];
+			//print_r($location_array);
+			//echo $_COOKIE['location'];
 			?>
 			<?php
 				// Populate dropdown with users if Law or Med
@@ -189,7 +189,6 @@ global $user_arrays;
 				<div class="col-md-12">
 					<p>
 						<a href="crud/list.php"><button type="button" class="btn btn-sm btn-WSUgreen"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Manage</button></a>
-						<a href="#" onclick="window.open('./index.php','ref_stats','menubar=0,resizable=0,width=350,height=880');"><button type="button" class="btn btn-sm btn-WSUgreen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span> Launch Pop-Up</button></a>
 						<a href="RefStats_Tool_Documentation.html" ><button type="button" class="btn btn-sm btn-WSUgreen"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></button></a>
 					</p>
 				</div>
